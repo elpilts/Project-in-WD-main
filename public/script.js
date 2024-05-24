7df4def3a09961075859ff348a6345c2154b40d0
katerinazk <katziaka2001@gmail.com>
2024-05-24 21:34:55 +0300

    added ekdhloseis js

--- public/script.js
+++ public/script.js
@@ -109,3 +109,152 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+
+//εκδηλώσεις
+//import { compareSync } from "bcrypt";
+
+let a=0;
+document.addEventListener('DOMContentLoaded', function() {
+    deleteDiv();
+    permanentDelete();
+    // changef();
+    insertf();
+    // heightClass();
+});
+
+// function heightClass(){
+//     const Divs = document.querySelectorAll('.events div');
+//     const allthedivs = [];
+//     Divs.forEach(div => {
+//         const divHeight = div.offsetHeight;
+//         allthedivs.push(divHeight);
+//     });
+//     console.log("length:", allthedivs.length);
+//     for (let i = 0; i < allthedivs.length; i++){
+//         if (i%2==0){
+//             console.log(i,allthedivs[i]);
+//         };
+//     }
+// };
+
+//Delete
+function deleteDiv(){
+    const del = document.querySelectorAll('.Delete');
+    del.forEach(button => {
+        button.addEventListener('click', function() {
+            const parentDiv = button.closest('div');
+            if (parentDiv) {
+                localStorage.setItem('deletedElementId', parentDiv.id); //για να διαγραφει μονιμα
+                parentDiv.remove();
+            }
+        });
+    });
+};
+
+//μονιμη διαγραφή
+function permanentDelete(){
+    const deletedElementId = localStorage.getItem('deletedElementId');
+    if (deletedElementId) {
+        // Remove the deleted element if it exists
+        const deletedElement = document.getElementById(deletedElementId);
+        if (deletedElement) {
+            deletedElement.remove();
+        }
+        // Clear the stored deleted element information
+        localStorage.removeItem('deletedElementId');
+    }
+};
+
+//Insert
+function insertf(){
+    permanentDelete();
+    let name = sessionStorage.getItem('eventName');
+    let description = sessionStorage.getItem('eventDescription');
+    let picture = sessionStorage.getItem('eventPicture');
+
+    if (name&&description&&picture&&a==0){
+        const newEvent = document.createElement('div');
+        const className = name.replace(/\s+/g, '-').toLowerCase();
+
+        newEvent.innerHTML = `
+            <div class="${className}">
+                <h2><em>${name}</em><button class="Edit" id="e-course">Edit</button><button class="Delete" id="d-course">Delete</button></h2>
+                <div class="con-${className}">
+                    <p>${description}</p>
+                    <img src="${picture}" alt="Image about ${name}">
+                </div>
+            </div>
+            <style>.con-${className}{
+                display: flex;
+                width: 80%;
+              }
+            //   .${className}::before{
+            //     content: "";
+            //     position: absolute;
+            //     left: 100px;
+            //     top: 40%;
+            //     width: 20px;
+            //     height: 20px;
+            //     background-color: #360f2a;
+            //     border-radius: 50%;
+              }
+            </style>
+        `;
+
+        const eventContainer = document.querySelector('.events');
+        eventContainer.appendChild(newEvent);
+        deleteDiv();
+        name="";
+        description=""
+        picture="";
+    }
+    a=0;
+};
+
+//Edit
+// function changef(){
+//     console.log("change a",a);
+//     const ed = document.querySelectorAll('.Edit');
+//     ed.forEach(button => {
+//         button.addEventListener('click', function() {
+//             let name = sessionStorage.getItem('eventName2');
+//             let description = sessionStorage.getItem('eventDescription2');
+//             let picture = sessionStorage.getItem('eventPicture2');
+//             console.log(name, description, picture);
+//             const parentDiv = button.closest('div'); 
+//             console.log("1",name,description,picture,parentDiv);
+//             if (parentDiv) {
+//                 const parentClassList = parentDiv.classList;
+//                 const className = parentClassList[0];
+//                 const theDiv = document.querySelector(`.${className}`);
+//                 if (theDiv) {
+//                     const classTitle = theDiv.querySelector('h2 em');
+//                     const classP = theDiv.querySelector('p');
+//                     const imgElement = theDiv.querySelector('img');
+//                     console.log("2",classTitle,classP,imgElement);
+//                     if (imgElement) {
+//                         const imgSrc = imgElement.src;
+//                         const classImg = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
+//                         if (name && classTitle){
+//                             classTitle.innerText = name;
+//                             console.log("name complete");
+//                         }
+//                         if(description && classP){
+//                             classP.innerText = description;
+//                             console.log("description complete");
+//                         }
+//                         if(picture && classImg){
+//                             classImg.innerText = picture;
+//                             console.log("picture complete");
+//                         }
+//                     }
+//                 }
+//                 a=1;
+//             }
+//             name="";
+//             description=""
+//             picture="";
+//         });
+//     });
+// };
