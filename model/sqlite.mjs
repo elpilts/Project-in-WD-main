3b486a5042c8eb255558ed664f3a137c33106f54
katerinazk <katziaka2001@gmail.com>
2024-05-26 13:01:50 +0300

    did the redirect successfully!

--- model/sqlite.mjs
+++ model/sqlite.mjs
@@ -43,41 +43,17 @@ export let Events = () => {
     }
 }
 
-// export let Insert = (eventName,eventDescription,eventPicture) => {
-//     const query = db.prepare('INSERT INTO Event (Name, Description, Picture) VALUES (?, ?, ?)'); //οριζει query
-//     let info;
-//     try { //trexei query & το επιστρέφει
-//         info = query.run(eventName,eventDescription,eventPicture);
-//         return true;
-//     }
-//     catch (err) {
-//         throw err;
-//     }
-// }
-
-// export let Delete = (deletename) => {
-//     const query = db.prepare('DELETE FROM Event WHERE Name = ?'); //οριζει query
-//     let info;
-//     try { //trexei query & το επιστρέφει
-//         info = query.run(deletename);
-//         return info;
-//     }
-//     catch (err) {
-//         throw err;
-//     }
-// }
-
-// export let Edit = (eventName, eventDescription, eventPicture) => {
-//     const query = db.prepare('UPDATE Event SET Description = ?, Picture = ? WHERE Name = ?'); //οριζει query
-//     let info;
-//     try { //trexei query & το επιστρέφει
-//         info = query.run(eventDescription, eventPicture, eventName);
-//         return true;
-//     }
-//     catch (err) {
-//         throw err;
-//     }
-// };
+export let Delete = (deletename) => {
+    const query = db.prepare('DELETE FROM Event WHERE Name = ?'); //οριζει query
+    let info;
+    try { //trexei query & το επιστρέφει
+        info = query.run(deletename);
+        return info;
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
 // Function to insert form data into the database
 export const insertFormData = (formData) => {
@@ -139,23 +115,3 @@ export const Insert = (eventData) => {
         db.close();
     }
 };
-
-// Function to insert user data
-//export const insertUserData = (name, email, phone, fieldOfStudies, password) => {
-    // Open database connection
-    //const db = openDatabaseConnection();
-    
-    //try {
-        // Prepare and execute the insert query
-        //const insertQuery = db.prepare('INSERT INTO Volunteer (Name, Email, Phone, Studies, Password) VALUES (?, ?, ?, ?, ?)');
-        //const info1 = insertQuery.run(name, email, phone, fieldOfStudies, password);
-       // console.log(info1);
-        //return info.changes; // Number of rows affected (should be 1 for successful insert)
-    //} catch (err) {
-        //console.error("Error inserting user data:", err);
-        //throw err;
-   // } finally {
-        // Close the database connection
-       // db.close();
-   // }
-//};
--- routes/router.mjs
+++ routes/router.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import session from 'express-session';
+import sqlite from 'better-sqlite3';
 
 const homeController = await import(`../controllers/Home.mjs`)
 const eventController = await import(`../controllers/Events.mjs`)
@@ -9,6 +10,13 @@ const editController = await import(`../controllers/Edit.mjs`)
 const signupController = await import('../controllers/Signup.mjs')
 import { InsertEvent } from '../controllers/Insert.mjs';
 import { EditEvent } from '../controllers/Edit.mjs';
+import { DeleteEvent } from '../controllers/Delete.mjs';
+// const con = await import('./connection');
+
+const openDatabaseConnection = () => {
+    const db = sqlite('db/Project_WebD_db.db', { verbose: console.log });
+    return db;
+};
 
 const router = express.Router()
 
@@ -70,17 +78,6 @@ router.get('/events', async (req,res) => {
     }
 });
 
-// router.post('/insert-event', async (req, res) => {
-//     // console.log('Request body:', req.body);
-//     // const { name, description, picture } = req.body;
-//     try {
-//         await insertController.InsertEvent(req,res);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
 router.get('/events/insert', async (req,res) => {
     try{
         res.render('NewEvent',{
@@ -99,80 +96,41 @@ router.get('/events/insert', async (req,res) => {
     }
 });
 
-router.delete('/events/:eventName', async (req, res) => {
-    const eventName = req.params.eventName;
-    console.log("name:", eventName);
-    try {
-        await deleteController.DeleteEvent( eventName );
-        res.sendStatus(204);
-    } catch (error) {
-        console.error('Error deleting event:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-// router.post('/events', async (req,res) => {
-//     const { thedelevent } = req.body;
-//     try{
-//         console.log("deleted elem:", thedelevent);
-//         const noevent = await deleteController.DeleteEvent(thedelevent);
-//     }
-//     catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-// router.put('/events/edit/:eventName', async (req, res) => {
+// router.delete('/events/:eventName', async (req, res) => {
 //     const eventName = req.params.eventName;
-//     const eventDescription = req.params.eventDescription;
-//     const eventPicture = req.params.eventPicture;
+//     console.log("name:", eventName);
 //     try {
-//         console.log("name:", eventName);
-//         await editController.EditEvent(eventName, eventDescription, eventPicture);
+//         await deleteController.DeleteEvent( eventName );
+//         res.sendStatus(204);
 //     } catch (error) {
 //         console.error('Error deleting event:', error);
 //         res.status(500).send('Internal Server Error');
 //     }
 // });
 
-// router.get('/events/edit', async (req,res) => {
-//     try{
-//         res.render('EditPage',{
-//             atHome: false,
-//             atAbout: false,
-//             atEvent: false,
-//             atContact: false,
-//             atAccount: false,
-//             atInsert: false,
-//             atEdit: true
-//         });
-//     }
-//     catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+router.get('/events/edit', async (req, res) => {
+    res.render('EditPage',{
+        atHome: false,
+        atAbout: false,
+        atEvent: false,
+        atContact: false,
+        atAccount: false,
+        atInsert: false,
+        atEdit: true
+    });
+});
 
-// router.get('/events/edit', async (req, res) => {
-//     res.render('EditPage',{
-//         atHome: false,
-//         atAbout: false,
-//         atEvent: false,
-//         atContact: false,
-//         atAccount: false,
-//         atInsert: false,
-//         atEdit: true
-//     });
-// });
-
-// router.post('/events/edit', async (req, res) => {
-//     try {
-//         await editController.EditEvent(req,res);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+router.get('/events/insert', async (req, res) => {
+    res.render('NewEvent',{
+        atHome: false,
+        atAbout: false,
+        atEvent: false,
+        atContact: false,
+        atAccount: false,
+        atInsert: true,
+        atEdit: false
+    });
+});
 
 router.get('/about', async (req,res) => {
     try{
@@ -270,7 +228,7 @@ router.post('/events/edit', function(request, response, next){
     // Call function to insert data into the database
     EditEvent(editData)
         .then(() => {
-            response.send('Data inserted successfully!');
+            response.redirect('/events')
         })
         .catch(error => {
             console.error('Error inserting data:', error);
@@ -319,7 +277,7 @@ router.post('/events/insert', function(request, response, next){
     // Call function to insert data into the database
     InsertEvent(eventData)
         .then(() => {
-            response.send('Data inserted successfully!');
+            response.redirect('/events')
         })
         .catch(error => {
             console.error('Error inserting data:', error);
@@ -327,52 +285,22 @@ router.post('/events/insert', function(request, response, next){
         });
 });
 
-//Insert Event
-// app.get('/events/insert', function(request, response, next){
-
-// 	response.send(`
-//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-// 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-//     <main>
-//     <div class="create">
-//         <h1> Create New Event</h1>
-//         <form id="createForm" action="/events/insert" method="POST">
-//             <div class="c-name">
-//                 <label for="name"> Name of the Event <br> </label>
-//                 <input type="text" id="name" name="name" placeholder="Event Name" required>
-//             </div>
-//             <div class="c-description">
-//                 <label for="description"> Description of the Event <br> </label>
-//                 <input type="text" id="description" name="description" placeholder="Description" required>
-//             </div>
-//             <div class="c-picture">
-//                 <label for="picture"> Picture of the Event <br> </label>
-//                 <input type="text" id="picture" name="picture" placeholder="Picture Link" required>
-//             </div>
-//             <div class="create-btn">
-//                 <input type="submit" value="Create" />
-//             </div>
-//         </form>
-//     </div>
-// </main>
-// 	`);
-// });
-
-// app.post('/events/insert', function(request, response, next){
-//     // Parse form data
-//     const eventData = {
-//         name: request.body.name,
-//         description: request.body.description,
-//         picture: request.body.picture
-//     };
-
-//     // Call function to insert data into the database
-//     insertController.InsertEvent(eventData)
-//         .then(() => {
-//             response.send('Data inserted successfully!');
-//         })
-//         .catch(error => {
-//             console.error('Error inserting data:', error);
-//             response.status(500).send('An error occurred while inserting data.');
-//         });
-// });
+router.delete('/:name', function(req, res, next){
+    const db = openDatabaseConnection();
+    let name = req.params.name;
+    // DeleteEvent(name)
+        // .then(() => {
+        //     res.send('Data deleted successfully!');
+        // })
+        // .catch(error => {
+        //     console.error('Error inserting data:', error);
+        //     res.status(500).send('An error occurred while inserting data.');
+        // });
+    db.query('DELETE FROM Event WHERE Name = ' + name, (err,result)=>{
+        if(err){
+            throw err;
+        } else{
+            res.send(result);
+        }
+    });
+});
